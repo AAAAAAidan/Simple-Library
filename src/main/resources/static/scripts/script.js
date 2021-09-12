2ae76edd24d22d71da9809385a0824ce06aa4d87
AAAAAAidan <a.k.zamboni@gmail.com>
2021-09-11 18:05:28 -0700

    Update general HTML/CSS, add list pop-up HTML/CSS/JS

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,10 +1,65 @@
-// Apply script to the search page
+var overlayId = "listOverlay";
+var uncheckedBox = "☐";
+var checkedBox = "☑";
+var itemId = "";
+var itemType = "";
+
+function openListPopup(id, type) {
+  document.getElementById(overlayId).style.display = "block";
+  document.getElementById(overlayId).addEventListener('click', function(e){checkForOverlayClick(e)});
+  itemId = id;
+  itemType = type;
+}
+
+function checkForOverlayClick(click) {
+  if (!document.getElementById('listPopup').contains(click.target)) {
+    closeListPopup();
+  }
+}
+
+function closeListPopup() {
+  document.getElementById(overlayId).style.display = "none";
+}
+
+function updateList(listId) {
+  var element = document.getElementById(listId);
+
+  if (element.innerHTML.indexOf(uncheckedBox) != -1) {
+    saveToList(listId);
+  }
+  else {
+    deleteFromList(listId);
+  }
+}
+
+function saveList() {
+  var newListId = document.getElementById("newListId").value;
+  console.log("Save lists/" + newListId)
+}
+
+function saveToList(listId) {
+  console.log("Save " + itemType + "/" + itemId + " to lists/" + listId)
+  var oldValue = document.getElementById(listId).innerHTML;
+  document.getElementById(listId).innerHTML = oldValue.replace(uncheckedBox, checkedBox);
+}
+
+function deleteList(listId) {
+  console.log("Delete lists/" + listId)
+}
+
+function deleteFromList(listId) {
+  console.log("Delete " + itemType + "/" + itemId + " from lists/" + listId)
+  var oldValue = document.getElementById(listId).innerHTML;
+  document.getElementById(listId).innerHTML = oldValue.replace(checkedBox, uncheckedBox);
+}
+
+// For the search page only
 if (document.location.pathname.match("search")) {
-  document.onclick = checkSelection;
+  document.addEventListener('click', checkResultsPerPageSelection);
 }
 
 // Check for changes to the results per page
-async function checkSelection() {
+function checkResultsPerPageSelection() {
   var oldSelection = document.getElementById("resultsPerPage").value;
   var newSelection = document.getElementById("resultsPerPageSelector").value;
 
